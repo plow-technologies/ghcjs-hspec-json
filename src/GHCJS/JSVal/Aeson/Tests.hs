c33014d85dd9ae492e424cffef0ebcbd1e186e5e
Sönke Hahn <soenkehahn@gmail.com>
2016-06-16 17:49:59 +0800

    implement generic test for ToJSVal

--- src/GHCJS/JSVal/Aeson/Tests.hs
+++ src/GHCJS/JSVal/Aeson/Tests.hs
@@ -1,2 +1,42 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module GHCJS.JSVal.Aeson.Tests where
+module GHCJS.JSVal.Aeson.Tests (
+  genericToJSValTests,
+
+  -- re-export
+  Proxy(..),
+ ) where
+
+import           Data.Aeson as Aeson
+import           Data.ByteString.Lazy (ByteString)
+import           Data.JSString
+import           Data.Proxy
+import           Data.String.Conversions
+import           Data.Typeable
+import           GHCJS.Marshal
+import           GHCJS.Types
+import           Test.Hspec
+import           Test.QuickCheck
+
+-- | fixme: mention HUnit and hspec
+genericToJSValTests :: forall a . (Typeable a, Show a, Arbitrary a, ToJSVal a, ToJSON a) =>
+  Proxy a -> Spec
+genericToJSValTests proxy = do
+  describe ("ToJSVal " ++ show (typeRep proxy)) $ do
+    it "converts to JSON compatible with aeson" $ do
+      property $ \ (a :: a) -> do
+        jsVal <- toJSVal a
+        jsValAsString <- unpack <$> jsonStringify jsVal
+        Aeson.encode a `shouldBeSameJson` cs jsValAsString
+
+foreign import javascript unsafe
+  "JSON.stringify($1)"
+  jsonStringify :: JSVal -> IO JSString
+
+shouldBeSameJson :: ByteString -> ByteString -> IO ()
+shouldBeSameJson a b = do
+  let dec :: ByteString -> Value
+      dec s = either (\ err -> error ("invalid json: " ++ err ++ "\n" ++ cs s)) id $
+        eitherDecode s
+  dec a `shouldBe` dec b
--- test/GHCJS/JSVal/Aeson/TestsSpec.hs
+++ test/GHCJS/JSVal/Aeson/TestsSpec.hs
@@ -1,10 +1,52 @@
+{-# LANGUAGE DeriveGeneric #-}
 
 module GHCJS.JSVal.Aeson.TestsSpec where
 
+import           Data.Aeson
+import           Data.Aeson.Types
+import           GHC.Generics
+import           GHCJS.Marshal
+import           System.IO
+import           System.IO.Temp
 import           Test.Hspec
+import           Test.Hspec.Core.Runner
+import           Test.QuickCheck
+
+import           GHCJS.JSVal.Aeson.Tests
+
+hspecSilently :: Spec -> IO Summary
+hspecSilently s = do
+  withSystemTempFile "ghcjs-hspec-jsval-aeson" $ \ path handle -> do
+    hClose handle
+    let silentConfig :: Test.Hspec.Core.Runner.Config
+        silentConfig = defaultConfig{
+          configOutputFile = Right path
+        }
+    hspecWithResult silentConfig s
 
 spec :: Spec
 spec = do
   describe "genericToJSValTests" $ do
-    it "" $ do
-      pending
+    it "detects faulty ToJSVal instances" $ do
+      summary <- hspecSilently $ genericToJSValTests faultyProxy
+      summary `shouldBe` Summary 1 1
+
+data Faulty
+  = Faulty {
+    faultyFoo :: String,
+    faultyBar :: String
+  }
+  deriving (Show, Generic)
+
+faultyProxy :: Proxy Faulty
+faultyProxy = Proxy
+
+instance ToJSVal Faulty
+instance ToJSON Faulty where
+  toJSON = genericToJSON defaultOptions{
+    fieldLabelModifier = drop (length "faulty")
+  }
+
+instance Arbitrary Faulty where
+  arbitrary = Faulty <$> arbitrary <*> arbitrary
+
